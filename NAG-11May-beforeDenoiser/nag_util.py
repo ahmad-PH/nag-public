ca801f95595027c2b8d76f50731a7df092e80790
ahmad pourihosseini <seyedahmadpourihosseini@gmail.com>
2019-06-28 13:35:41 +0430

    Add validation functin

--- NAG-11May-beforeDenoiser/nag_util.py
+++ NAG-11May-beforeDenoiser/nag_util.py
@@ -522,3 +522,15 @@ class DiversityWeightsScheduler(LearnerCallback):
       self.weight = 1.
       
     self.learn.loss_func.weights = [self.weight] * len(layers)
+
+
+def validation(gen_output, target):
+    perturbations, clean_images = gen_output
+    perturbed_images = clean_images + perturbations
+#     print("perturbed image size: %s, dis_input size: %s and perturbation size: %s"%(
+#         prturbed_img.shape, dis_input.shape, perturbation.shape
+#     ))
+    benign_preds = torch.argmax(arch(clean_images), 1)
+    adversary_preds = torch.argmax(arch(perturbed_images), 1)
+    
+    return (benign_preds != adversary_preds).float().mean()
